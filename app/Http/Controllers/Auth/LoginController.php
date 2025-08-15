3bdbaee6f6d558145969edb60dfd6452fad373ec
A1n2F <alissonfferreira22@gmail.com>
2025-08-15 14:23:10 -0300

    Refatoração do Form Request

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\MakeLoginRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,20 +14,11 @@ class LoginController extends Controller
         return view('auth.login');
     }
 
-    public function login() {
-        request()->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required']
-        ]);
-
-        if($user = User::query()->where('email', '=', request()->email)->first()) {
-            if(Hash::check(request()->password, $user->password)) {
-                auth()->login($user);
-
-                return to_route('dashboard');
-            };
-        };
-
+    public function login(MakeLoginRequest $request) {
+        if($request->attempt()) {
+            return to_route('dashboard');
+        }
+        
         return back()->with(['message'=>'Não deu certo!']);
     }
 }
