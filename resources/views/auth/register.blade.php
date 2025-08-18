5a51e294077d9458657556af0debf6027c65387b
A1n2F <alissonfferreira22@gmail.com>
2025-08-18 10:54:38 -0300

    Registrando um usuário

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,18 +1,33 @@
 <div>
-    <h1>LOGIN</h1>
+    <h1>REGISTER</h1>
 
     @if($message = session()->get('message'))
         <div>{{ $message }}</div>
     @endif
 
-    <form action="{{ route('login') }}" method="post">
+    <form action="{{ route('register') }}" method="post">
         @csrf
+
+        <div>
+            <input name="name" placeholder="Name" value="{{ old('name') }}" />
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <br>
 
         <div>
             <input name="email" placeholder="Email" value="{{ old('email') }}" />
             @error('email')
                 <span>{{ $message }}</span>
             @enderror
+        </div>
+
+        <br>
+
+        <div>
+            <input name="email_confirmation" placeholder="Email Confirmation" />
         </div>
 
         <br>
@@ -26,6 +41,6 @@
 
         <br>
 
-        <button>Logar</button>
+        <button>Registrar</button>
     </form>
 </div>
